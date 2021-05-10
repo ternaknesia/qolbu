0f4731d35b43014133b2b0d6945c7255eece573a
Suryawan Bimantoro <stanleybimantoro@gmail.com>
2021-05-10 16:47:05 +0700

    change database

--- config/database.go
+++ config/database.go
@@ -18,26 +18,29 @@ func configDatabase(configuration Config, target string) (string, string, string
 	return dbHost, dbUser, dbPass, dbName
 }
 
-func createDSN(configuration Config, target string) string {
+func createDSN(configuration Config, target string) (string, string) {
 	dbHost, dbUser, dbPass, dbName := configDatabase(configuration, target)
-	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=true&loc=Local", dbUser, dbPass, dbHost, dbName)
+	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=true&loc=Local", 
+		dbUser, dbPass, dbHost, dbName),
+		dbName
 }
 
-func openDatabase(ctx context.Context, configuration Config, target string) *sql.DB {
-	db, err := sql.Open("mysql", createDSN(configuration, target))
+func openDatabase(ctx context.Context, configuration Config, target string) (*sql.DB, string) {
+	dsn, dbName := createDSN(configuration, target)
+	db, err := sql.Open("mysql", dsn)
 	exception.PanicIfNeeded(err)
 
 	err = db.PingContext(ctx)
 	exception.PanicIfNeeded(err)
 
-	return db
+	return db, dbName
 }
 
 func CreateContext() (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), 300*time.Second)
 }
 
-func CreateDatabase(configuration Config, target string) *sql.DB {
+func CreateDatabase(configuration Config, target string) (*sql.DB, string) {
 	ctx, cancel := CreateContext()
 	defer cancel()
 	return openDatabase(ctx, configuration, target)
